b003638ab107e3661ebb2883ed798750e00d3c19
Darkoeur <Antoine.Gautrain@gmail.com>
2018-04-29 15:17:08 +0200

    minor patch: advantaging player

--- Admin/src/app/admin.service.ts
+++ Admin/src/app/admin.service.ts
@@ -6,89 +6,89 @@ import { Claim, Player, Challenge, Team, Note } from './interfaces';
 
 import { environment } from '../environments/environment';
 
-const server: string = environment.remoteApiAddress + ':' + environment.remoteApiPort + '/';
+const server: string = 'http://' + environment.remoteApiAddress + ':' + environment.remoteApiPort + '/';
 
 @Injectable()
 export class AdminService {
-    
+
     claims: Claim[] = [];
     claimEdited: Claim;
 
   constructor(private http: Http) { }
-    
+
     list(): Promise<Claim[]> {
         console.log('requesting /admin/list');
-        
+
         return this.http.get(server + 'admin/list')
             .map(response => response.json())
             .toPromise();
     }
-    
+
     accept(comment: string): Promise<any> {
         console.log('requesting /admin/accept');
-        
+
         return this.http.get(server + 'admin/accept?id=' + this.claimEdited.claimer.id + '&claim=' + this.claimEdited.id + '&comment=' + comment)
             .map(response => response.json())
             .toPromise();
     }
-    
+
     refuse(comment: string): Promise<any> {
         console.log('requesting /admin/refuse');
-        
+
         return this.http.get(server + 'admin/refuse?id=' + this.claimEdited.claimer.id + '&claim=' + this.claimEdited.id + '&comment=' + comment)
             .map(response => response.json())
             .toPromise();
     }
-    
+
     teams(): Promise<Team[]> {
         console.log('requesting /admin/teams');
         return this.http.get(server + 'admin/teams')
             .map(response => response.json())
             .toPromise();
     }
-    
+
     players(): Promise<Player[]> {
         console.log('requesting /admin/players');
         return this.http.get(server + 'admin/players')
             .map(response => response.json())
             .toPromise();
     }
-    
+
     notes(): Promise<{ [key: string]: Note[] }> {
         console.log('requesting /admin/notes');
         return this.http.get(server + 'admin/notes')
             .map(response => response.json())
             .toPromise();
     }
-    
+
     advantage(content): Promise<any> {
         console.log('posting /admin/advantage');
         let headers = new Headers({'Content-Type':'application/json'});
         let options = new RequestOptions({headers:headers});
-        
+
         return this.http.post(server + 'admin/advantage', content, options)
             .map(response => response.json())
             .toPromise();
     }
-    
+
     advantagePlayer(content): Promise<any> {
         console.log('posting /admin/advantage/player');
         let headers = new Headers({'Content-Type':'application/json'});
         let options = new RequestOptions({headers:headers});
-        
+
         return this.http.post(server + 'admin/advantage/player', content, options)
             .map(response => response.json())
             .toPromise();
     }
-    
+
     setEdited(claim: Claim): void {
         this.claimEdited = claim;
     }
-    
+
     getEdited(): Claim {
         return this.claimEdited;
     }
-    
+
 
 
 }
